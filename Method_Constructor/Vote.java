ed988d5cd6bf1330aa6dd499e93fa3e360461f7c
ssrajkiran <ssrajkiran1@gmail.com>
2020-07-08 16:44:57 +0530

    updated

---
+++ Method_Constructor/Vote.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.io.Console;
+class Vote
+{
+    public void name(String name)
+    {
+        System.out.println("welcome:"+name); 
+        
+    }
+    public void age(int age)
+    {
+        if(age<18)
+        {
+            System.out.println("You are not allowed..");
+        }  
+        else
+        {
+            System.out.println("You are allowed..");
+        }
+    }
+
+    public static void main(String args[])throws Exception
+    {
+        InputStreamReader r = new InputStreamReader(System.in);
+        BufferedReader input   = new BufferedReader(r);
+
+        System.out.println("Enter your name:");
+        String name = input.readLine();
+
+        System.out.println("Enter your age:");
+        int age = Integer.parseInt(input.readLine());
+
+
+        
+        Vote n = new Vote();
+        Vote a = new Vote();
+        n.name(name);
+        a.age(age);
+        
+    }
+}
